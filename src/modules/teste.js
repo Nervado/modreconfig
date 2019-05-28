be1ba6fa16237ecae1a94a5e815b4fa0c209265c
Nervado <evandroabreudeoliveira@gmail.com>
2019-05-28 18:02:55 -0300

    path adjustments

---
+++ src/modules/teste.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+console.log(path.resolve(`public/out/pn0${1}/`, 'C-1.txt'))
+
+//C:\Users\adm\Desktop\DEV\modreconfig\src\assets\lists
+
+//c:\Users\adm\Desktop\DEV\modreconfig\C-1.txt
+
+//../assets/lists/C-98.txt'
+
--- src/modules/testeCriaFc.js
+++ src/modules/testeCriaFc.js
@@ -1,4 +1,5 @@
 //imports 
+const path = require('path')
 const redes = require('./criaRede')
 const cp = require('./analisaCP')
 const dbL = require('./criaDBLeituras')
@@ -15,9 +16,10 @@ let vetorRedes = []
 let k = 0
 remota.numCPs.forEach(function(numCP){    
     //console.log(k)
-    let arquivoCP = `/C-${numCP}.txt`
-    let path =  __dirname + arquivoCP
-    let data = fs.readFileSync(path,'utf-8')
+    let arquivoCP = `C-${numCP}.txt`
+    //let path =  __dirname + arquivoCP
+    let _path = path.resolve('src/assets/lists', arquivoCP)
+    let data = fs.readFileSync(_path,'utf-8')
     data = data.trim()
     data = data.split(/\r?\n/)   
     console.log('Encontrado: '+ arquivoCP)
@@ -31,30 +33,33 @@ remota.numCPs.forEach(function(numCP){
 k = 0
 remota.numFCs.forEach(numFC => {
     let fc 
-    let arquivoFC = `/${remota.nome.split('a')[1]}FC${numFC}.txt`
-    let path = __dirname + arquivoFC
-    let data = fs.readFileSync(path,'utf-8')
+    let arquivoFC = `${remota.nome.split('a')[1]}FC${numFC}.txt`
+    //let path = __dirname + arquivoFC
+    let _path = path.resolve('src/assets/oldfcs',arquivoFC)
+    let data = fs.readFileSync(_path,'utf-8')
     //console.log(arquivoFC)
     fc = fcMake.setFC(vetorRedes,data.split(/\r?\n/),numFC)
-    path = __dirname + `\\${remota.nome.split('a')[1]}FC${numFC}new.scl`
+    //_path = __dirname + `\\${remota.nome.split('a')[1]}FC${numFC}new.scl`
+    _path = path.resolve(`public/out/pn${remota.nome.split('N')[1]}/`,`${remota.nome.split('a')[1]}FC${numFC}new.scl`) 
+    //path.resolve(`public/out/pn0${1}/`, 'C-1.txt'))
     if(fc === ''){}
     else{
         //salvar arquivo no diretorio local
-        fs.writeFile(path, fc,function(erro){
+        fs.writeFile(_path, fc,function(erro){
             if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
+            console.log(`Arquivo salvo em: `,_path)
             //console.log(fc)
         })
 
         //salvar arquivo no diretorio de saida
-        path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
-        let fileName = `\\${remota.nome.split('a')[1]}FC${numFC}new.scl`
-        path = path + fileName
-        fs.writeFile(path, fc,function(erro){
-            if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
-            //console.log(fc)
-        })
+        //path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
+        //let fileName = `\\${remota.nome.split('a')[1]}FC${numFC}new.scl`
+        //path = path + fileName
+        //fs.writeFile(path, fc,function(erro){
+        //    if(erro){ throw erro}
+        //    console.log(`Arquivo salvo em: `,path)
+        //    //console.log(fc)
+        //})
 
     }
 })
@@ -62,27 +67,28 @@ remota.numFCs.forEach(numFC => {
 k = 0
 if(remota.numDBsLeituras.length === vetorRedes.length){//verificar se o numero de CPs analisadas é igual a quanitade de datablocks definidos
     remota.numDBsLeituras.forEach(num => { //para cada rede definida cria a DB de requisições de leitura
-        let arquivoDB = `\\DB${num}.scl`
-        let path = __dirname + arquivoDB
+        let arquivoDB = `DB${num}.scl`
+        let _path = path.resolve(`public/out/pn${remota.nome.split('N')[1]}/`, arquivoDB) 
+        //let path = __dirname + arquivoDB
         let db = dbL.escreveDBLeituras(vetorRedes[k],num)
         let sclFonteDB = dbL.setDBLeituras(db,num,1)         
         k++
         // salvar arquivo no diretorio local 
-        fs.writeFile(path,sclFonteDB,function(erro){
+        fs.writeFile(_path,sclFonteDB,function(erro){
             if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
+            console.log(`Arquivo salvo em: `,_path)
             //console.log(sclFonteDB)
         })
 
         //Salvar arquivo no diretorio de saida         
-        arquivoDB = `\\\\DB${num}.scl`
-        path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
-        path = path + arquivoDB
-        fs.writeFile(path,sclFonteDB,function(erro){
-            if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
-            //console.log(dbDTRCV)
-        })
+        //arquivoDB = `\\\\DB${num}.scl`
+        //path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
+        //path = path + arquivoDB
+        //fs.writeFile(path,sclFonteDB,function(erro){
+        //    if(erro){ throw erro}
+        //    console.log(`Arquivo salvo em: `,path)
+        //    //console.log(dbDTRCV)
+        //})
 
 
     })
@@ -92,26 +98,27 @@ if(remota.numDBsLeituras.length === vetorRedes.length){//verificar se o numero d
 k = 0
 if(remota.numDBsEscritas.length === vetorRedes.length){//verificar se o numero de CPs analisadas é igual a quanitade de datablocks definidos
     remota.numDBsEscritas.forEach(num => { //para cada rede definida cria a DB de requisições de escrita
-        let arquivoDB = `\\DB${num}.scl`
-        let path = __dirname + arquivoDB
+        let arquivoDB = `DB${num}.scl`
+        let _path = path.resolve(`public/out/pn${remota.nome.split('N')[1]}/`, arquivoDB) 
+        //let path = __dirname + arquivoDB
         let db = dbW.setDBEscritas(vetorRedes[k],num)
         k++
         // salvar arquivo no diretorio local 
-        fs.writeFile(path,db,function(erro){
+        fs.writeFile(_path,db,function(erro){
             if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
+            console.log(`Arquivo salvo em: `,_path)
             //console.log(sclFonteDB)
         })
 
         //Salvar arquivo no diretorio de saida         
-        arquivoDB = `\\\\DB${num}.awl`
-        path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
-        path = path + arquivoDB
-        fs.writeFile(path,db,function(erro){
-            if(erro){ throw erro}
-            console.log(`Arquivo salvo em: `,path)
-            //console.log(dbDTRCV)
-        })
+        //arquivoDB = `\\\\DB${num}.awl`
+        //path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
+        //path = path + arquivoDB
+        //fs.writeFile(path,db,function(erro){
+        //    if(erro){ throw erro}
+        //    console.log(`Arquivo salvo em: `,path)
+        //    //console.log(dbDTRCV)
+        //})
     })
 }
 //Fim da criacao de DBS de comandos de escrita
@@ -145,24 +152,25 @@ BEGIN
 END_DATA_BLOCK
 `
     //salvar arquivo DB com os dados recebidos ...fonte em .SCL
-    let arquivoDBRCVDT = `\\DB${numDBRCVDT}.scl`
-    let path = __dirname + arquivoDBRCVDT 
+    let arquivoDBRCVDT = `DB${numDBRCVDT}.scl`
+    let _path = path.resolve(`public/out/pn${remota.nome.split('N')[1]}/`, arquivoDBRCVDT)
+    //let path = __dirname + arquivoDBRCVDT 
     
     //Salva no diretorio atual
-    fs.writeFile(path,dbDTRCV,function(erro){
+    fs.writeFile(_path,dbDTRCV,function(erro){
         if(erro){ throw erro}
-        console.log(`Arquivo salvo em: `,path)
+        console.log(`Arquivo salvo em: `,_path)
         //console.log(dbDTRCV)
     })
     //Salva uma copia do diretorio de saida 
-    arquivoDBRCVDT = `\\DB${numDBRCVDT}.scl`
-    path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
-    path = path + arquivoDBRCVDT
-    fs.writeFile(path,dbDTRCV,function(erro){
-        if(erro){ throw erro}
-        console.log(`Arquivo salvo em: `,path)
-        //console.log(dbDTRCV)
-    })
+    //arquivoDBRCVDT = `\\DB${numDBRCVDT}.scl`
+    //path = `C:\\Users\\Evandro\\Desktop\\Projeto Remoção C264 P53\\Logica Nova\\PN${diretorio}`
+    //path = path + arquivoDBRCVDT
+    //fs.writeFile(path,dbDTRCV,function(erro){
+    //    if(erro){ throw erro}
+    //    console.log(`Arquivo salvo em: `,path)
+    //    //console.log(dbDTRCV)
+    //})
 
 
     console.log('Scan total time '+`Network ${i++}`,totalizaTempo.toFixed(2), 'ms')

1451878df289c6d8a4ed099428107ac3211db716
Nervado <evandroabreudeoliveira@gmail.com>
2019-05-28 13:58:41 -0300

    add basic framing

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("ola electron!")
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+    // create the window
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences :{
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.on('ready',createWindow)
+
+
+
+
+
